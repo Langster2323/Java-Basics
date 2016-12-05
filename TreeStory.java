312e97f2dfc9bbb7d3a4453b484f82e7bd26ded1
Ahkeem Lang <langahkeem@gmail.com>
2016-12-05 09:21:29 -0600

    Created a conditional statment.

--- TreeStory.java
+++ TreeStory.java
@@ -5,6 +5,10 @@ public class TreeStory {
     public static void main(String[] args) {
         Console console = System.console();
       //__Name__ is a __adjective__ __noun__. They are always __adverb__ __verb__.
+      if (age < 13) {
+        console.printf("Sorry you must be at least 13 to use this program\n");
+        System.exit(0);
+      }
       String name = console.readLine("Enter a name:  ");
       String adjective = console.readLine("Enter an adjective:  ");
       String noun = console.readLine("Enter a noun:  ");
