b214dd46981ce94fec8d829a2e1e737a40a6f8d9
Ahkeem Lang <langahkeem@gmail.com>
2016-12-05 10:53:36 -0600

    Refactored my code and used a do while loop for my condition statement. I also used a glabal scope for the variable noun and isinvalidword.

--- TreeStory.java
+++ TreeStory.java
@@ -7,21 +7,30 @@ public class TreeStory {
       //__Name__ is a __adjective__ __noun__. They are always __adverb__ __verb__.
       String ageAsString = console.readLine("How old are you?  ");
       int age = Integer.parseInt(ageAsString);
-      if (age < 13) {
-        console.printf("Sorry you must be at least 13 to use this program\n");
-        System.exit(0);
-      }
-      String name = console.readLine("Enter a name:  ");
-      String adjective = console.readLine("Enter an adjective:  ");
-      String noun = console.readLine("Enter a noun:  ");
-      if (noun.equalsIgnoreCase("dork") ||
-          noun.equalsIgnoreCase("jerk") ||
-          noun.equalsIgnoreCase("nerd"));
-      String adverb = console.readLine("Enter an adjective:  ");
-      String verb = console.readLine("Enter a verb ending in -ing:  ");
-      console.printf("Your TreeStory:\n----------------\n");
-      console.printf("%s is a %s %s. ", name, adjective, noun);
-      console.printf("Thay are always %s %s.\n", adverb, verb);
-    }
+      if (age < 13){
+              //Insert exit code
+              console.printf("Sorry you must be at least 13 to use this program.\n");
+              System.exit(0);
+            }
+            String name = console.readLine("Enter a name:  ");
+            String adjective = console.readLine("Enter an adjective:  ");
+            // Below I'm scoping && refactored my code
+            String noun;
+            boolean isInvalidWord;
+            do {
+              noun = console.readLine("Enter a noun:  ");
+              isInvalidWord =(noun.equalsIgnoreCase("dork") ||
+                  noun.equalsIgnoreCase("jerk") ||
+                  noun.equalsIgnoreCase("nerd"));
+              if  (isInvalidWord) {
+                console.printf("That language is not allowed. Try again. \n\n");
+              }
+            } while (isInvalidWord);
+            String adverb = console.readLine("Enter an adjective:  ");
+            String verb = console.readLine("Enter a verb ending in -ing:  ");
+            console.printf("Your TreeStory:\n----------------\n");
+            console.printf("%s is a %s %s. ", name, adjective, noun);
+            console.printf("Thay are always %s %s.\n", adverb, verb);
+          }
 
-}
+      }
