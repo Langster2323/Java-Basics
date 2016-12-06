e2c4eab9e43ad37e3ecf8c401495d2aa38ba67b3
Ahkeem Lang <langahkeem@gmail.com>
2016-12-06 10:12:56 -0600

    Created a new instance of PezDispenser

--- Example.java
+++ Example.java
@@ -2,6 +2,10 @@ public class Example {
 
     public static void main(String[] args) {
         // Your amazing code goes here...
-
+        //println prints a formatted text with \n at the end of it
+        System.out.println("We are making a Pez Dispenser.");
+        PezDispenser dispenser = new PezDispenser();
+        System.out.printf("The dispenser character is %s\n",
+        dispenser.mCharacterName)
     }
 }
