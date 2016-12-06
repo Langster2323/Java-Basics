7e0be450b7a7becbaa9712d369a9f7c723914f51
Ahkeem Lang <langahkeem@gmail.com>
2016-12-06 10:34:38 -0600

    Added a getter, that can retrieve the character name

--- PezDispenser.java
+++ PezDispenser.java
@@ -3,4 +3,9 @@ public class PezDispenser {
   // "m" is a common naming prefix used for member variables
   //Changing the access level modifier to private
   private String mCharacterName = "Yoda";
+
+// Added a getter, that can be used to get the character name
+  public String getCharacterName() {
+    return mCharacterName;
+  }
 }
