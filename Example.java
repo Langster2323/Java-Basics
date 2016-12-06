26af020db723fa56c68131ab74ca27cf2c83a2cf
Ahkeem Lang <langahkeem@gmail.com>
2016-12-06 10:14:19 -0600

    Creating notes

--- Example.java
+++ Example.java
@@ -4,6 +4,7 @@ public class Example {
         // Your amazing code goes here...
         //println prints a formatted text with \n at the end of it
         System.out.println("We are making a Pez Dispenser.");
+        //Made a brand new Pez Dispenser...
         PezDispenser dispenser = new PezDispenser();
         System.out.printf("The dispenser character is %s\n",
         dispenser.mCharacterName)
