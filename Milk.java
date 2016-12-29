9fd928700a35d199fa35e0b30126929784dcb50a
Ahkeem Lang <langahkeem@gmail.com>
2016-12-29 14:48:09 -0600

    Added a comment

--- Milk.java
+++ Milk.java
@@ -5,7 +5,7 @@ public class Milk {
   public static void main(String[] args) {
     Console console = System.console();
 
-
+    //initialized; terminated; incremented.
     for (int i = 99; i > 0; i--) {
       console.printf("%d bottles of milk on the wall... %n", i);
     }
